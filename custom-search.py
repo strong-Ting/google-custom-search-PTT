4aeb77747bac67374a66ed969d201a81815c9500
strong-Ting <s8527502987@gmail.com>
2017-08-13 03:36:22 +0800

    2017/08/13  AM 03:36: ADD: -analysis content to get title and ip -how long is function executing TO

--- custom-search.py
+++ custom-search.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from googleapiclient.discovery import build
+import time
 
 def getService():
     service = build("customsearch", "v1",
@@ -35,6 +36,8 @@ def google_custom_search_api(q,pageLimit):
     return link_data
 
 def google_custom_search_crawler(query,page_limit):
+    start_time = time.time()
+
     result_index_limit = int(page_limit)*10
     result_index = 0
     response = []
@@ -70,10 +73,13 @@ def google_custom_search_crawler(query,page_limit):
 
         except KeyError:
             print("have error")
-    
+    end_time = time.time()
+    time_cost = end_time - start_time
+    print("custom:",time_cost)
     return link_data
 
 def crawler_content(link_data): #path should rename content_link
+    start_time = time.time()
     content = [] 
 
     for link in link_data:
@@ -82,12 +88,60 @@ def crawler_content(link_data): #path should rename content_link
         res = requests.post("https://www.ptt.cc/ask/over18",data=payload)
         res.encoding = 'usf8'
         content.append(res.text)
-
+    end_time = time.time()
+    time_cost = end_time - start_time
+    print("crawler_content:",time_cost)
     return content
 
+def analysis_content(content_list):
+    start_time = time.time()
+    title = []
+    ip = []
+    ID_content = []
+    date = []
+    board = []
+
+    title_index_start ='<span class="article-meta-tag">標題</span><span class="article-meta-value">' 
+    title_index_end = "</span>"
+    ip_index_start='<span class="f2">※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ' 
+    ip_index_end='</span>' 
+    ''' 
+    title_end = 100000
+    content = content_list[0]
+    title_start = content.index(title_index_start,title_end) + len(title_index_start)
+    title_end = content.index(title_index_end,title_start)
+    print(content[title_start:title_end])
+    '''     
+        
+    for content in content_list:
+        title_end  = 0
+        title_start = 0
+        ip_start=0
+        ip_end=0
+
+        exception = False
+        while exception != True:
+            try:
+                title_start = content.index(title_index_start,title_end) + len(title_index_start)
+                title_end = content.index(title_index_end,title_start)
+                ip_start = content.index(ip_index_start,ip_end)+len(ip_index_start)
+                ip_end = content.index(ip_index_end,ip_start)
+                ip.append(content[ip_start:ip_end].strip('\n'))
+ #                print(content[title_start:title_end])
+                title.append(content[title_start:title_end])
+            except ValueError:
+                exception = True
+    end_time =time.time()
+    time_cost=end_time-start_time
+    print("analysis:",time_cost)
+    return title,ip,ID_content,date,board
 
 
-
-
-print(crawler_content(google_custom_search_crawler("heaviest",1)))
-
+#f = open("./content.html","w")
+#f.write(crawler_content(google_custom_search_crawler("heaviest",1)))
+start_time = time.time()
+analysis_content(crawler_content(google_custom_search_crawler("heaviest",10)))
+#print(analysis_content(crawler_content(google_custom_search_crawler("heaviest",1))))
+end_time = time.time()
+time_cost=end_time-start_time
+print("sum:",time_cost)
